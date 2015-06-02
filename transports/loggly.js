3d66d28b454897a1af447354167f632e4044339b
David Roncancio <david.roncancio@gmail.com>
2015-06-02 12:39:13 -0500

    Hardcoding :S

--- transports/loggly.js
+++ transports/loggly.js
@@ -1,11 +1,11 @@
 var Loggly = require('winston-loggly').Loggly
-var levels = require('config').get('log').levels
+var config = require('config').get('log')
 var env = process.env.NODE_ENV || 'development'
 var appName = process.env.npm_package_name || 'default'
 
 module.exports = new (Loggly)({
-  subdomain: 'pagerinc',
-  level: levels.loggly || 'debug',
+  subdomain: config.domain || 'Use your loggly domain',
+  level: config.levels.loggly || 'debug',
   json: true,
   inputName: appName + '@' + env,
   inputToken: process.env.LOGGLY_TOKEN || 'Don\'t forget the token',
