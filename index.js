89613fbd4d9fea04b264f31d69e04ec8962bca55
Gaston Festari <cilindrox@gmail.com>
2015-06-11 17:30:33 -0300

    Parameterize default config

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+var path = require('path')
+var config = require('config')
+
 /**
  * Suppress missing config warnings.
  * See: https://github.com/lorenwest/node-config/wiki/Sub-Module-Configuration
  */
 process.env.SUPPRESS_NO_CONFIG_WARNING = 'y'
 
-var config = require('config')
+var appName = process.env.npm_package_name || 'default'
 
-var myDefaultConfigs = require('./config/' + (process.env.NODE_ENV || 'default') + '.json')
+var configDefaults = {
+  levels: {
+    console: 'info',
+    file: 'debug',
+    sentry: 'error',
+    loggly: 'info'
+  },
+  domain: 'pager',
+  buffers: 1,
+  path: path.join('/var/log', appName + '.log'),
+  period: '1d',
+  count: 3
+}
 
 module.exports = (function () {
-  config.util.setModuleDefaults('log', myDefaultConfigs)
+  config.util.setModuleDefaults('log', configDefaults)
   return require('./lib/logger')
 })()
